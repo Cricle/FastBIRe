ad906b5056309ec816f32630313fbcabd7a1fd8f
HCricle <none>
2023-10-07 21:47:32 +0800

    Add ducks func

--- samples/FastBIRe.Duck/Program.cs
+++ samples/FastBIRe.Duck/Program.cs
@@ -9,24 +9,26 @@ namespace FastBIRe.Duck
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var builder = new DuckDBConnectionStringBuilder();
             builder.DataSource = "q.duckdb";
             var duckConn = new DuckDBConnection(builder.ConnectionString);
             duckConn.Open();
-            //var table = new DatabaseTable
-            //{
-            //    Name = "test"
-            //};
-            //table.AddColumn("id").SetTypeDefault(SqlType.DuckdDB, DbType.Int32).AddIdentity();
-            //table.AddColumn("a1").SetTypeDefault(SqlType.DuckdDB, DbType.DateTime).AddIndex("IDX_11");
-            //table.AddColumn("a2").SetTypeDefault(SqlType.DuckdDB, DbType.Double);
-            //table.AddColumn("a3").SetTypeDefault(SqlType.DuckdDB, DbType.Decimal);
-            //table.AddColumn("a4").SetTypeDefault(SqlType.DuckdDB, DbType.Boolean);
-            //table.PrimaryKey = new DatabaseConstraint { Columns = { table.Columns[0].Name } };
-            //var ddl = new DdlGeneratorFactory(SqlType.DuckdDB).TableGenerator(table).Write();
-            //Console.WriteLine(ddl);
+            var executer = new DefaultScriptExecuter(duckConn);
+            var table = new DatabaseTable
+            {
+                Name = "test"
+            };
+            table.AddColumn("id").SetTypeDefault(SqlType.DuckdDB, DbType.Int32).AddIdentity();
+            table.AddColumn("a1").SetTypeDefault(SqlType.DuckdDB, DbType.DateTime).AddIndex("IDX_11");
+            table.AddColumn("a2").SetTypeDefault(SqlType.DuckdDB, DbType.Double);
+            table.AddColumn("a3").SetTypeDefault(SqlType.DuckdDB, DbType.Decimal);
+            table.AddColumn("a4").SetTypeDefault(SqlType.DuckdDB, DbType.Boolean);
+            table.PrimaryKey = new DatabaseConstraint { Columns = { table.Columns[0].Name } };
+            var ddl = new DdlGeneratorFactory(SqlType.DuckdDB).TableGenerator(table).Write();
+            Console.WriteLine(ddl);
+            await executer.ExecuteAsync(ddl);
             var reader = new DatabaseReader(duckConn);
             var tb = reader.Table("test");
             var cptb = reader.Table("test");
@@ -34,8 +36,9 @@ namespace FastBIRe.Duck
             tb.Columns[1].Id = 1;
 
             cptb.Columns[1].Name = "qweqewdawda";
-            var ddl = CompareSchemas.FromTable(builder.DataSource, SqlType.DuckdDB, tb, cptb).Execute();
+            ddl = CompareSchemas.FromTable(builder.DataSource, SqlType.DuckdDB, tb, cptb).Execute();
             Console.WriteLine(ddl);
+            await executer.ExecuteAsync(ddl);
         }
     }
 }
---
+++ src/FastBIRe/DuckDB/Copy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastBIRe.DuckDB
+{
+    public static class Copy
+    {
+        public static string CreateTableWithCsv(string tableName,string csvPath)
+        {
+            return $"CREATE TABLE \"{tableName}\" AS SELECT * FROM '{csvPath}';";
+        }
+        public static string ExportToCsv(string tableName, string csvPath)
+        {
+            return $"COPY (FROM \"{tableName}\") TO '{csvPath}' WITH (HEADER 1, DELIMITER '|');";
+        }
+    }
+}
