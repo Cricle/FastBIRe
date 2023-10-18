1e9b6e7b805676cfd7b814c14b406dc4176987c0
HCricle <15218450198@163.com>
2023-10-18 14:08:24 +0800

    v1.2.0-preview.5

--- src/FastBIRe.Cdc/CdcLogService.cs
+++ src/FastBIRe.Cdc/CdcLogService.cs
@@ -1,30 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace FastBIRe.Cdc
 {
-    public class CdcLog : Dictionary<string, object>, ICdcLog
-    {
-        public CdcLog(string name, ulong? length)
-            : base(StringComparer.OrdinalIgnoreCase)
-        {
-            Name = name;
-            Length = length;
-        }
-
-        public string Name { get; }
-
-        public ulong? Length { get; }
-    }
-    public interface ICdcLog : IDictionary<string, object>
-    {
-        string Name { get; }
-
-        ulong? Length { get; }
-    }
     public abstract class CdcLogServiceBase : ICdcLogService
     {
         public abstract Task<IList<ICdcLog>> GetAllAsync(CancellationToken token = default);
@@ -37,11 +17,5 @@ namespace FastBIRe.Cdc
                 log[reader.GetName(i)] = reader[i];
             }
         }
-    }
-    public interface ICdcLogService
-    {
-        Task<IList<ICdcLog>> GetAllAsync(CancellationToken token = default);
-
-        Task<ICdcLog?> GetLastAsync(CancellationToken token = default);
     }
 }
---
+++ src/FastBIRe.Cdc/ICdcLog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FastBIRe.Cdc
+{
+    public interface ICdcLog : IDictionary<string, object>
+    {
+        string Name { get; }
+
+        ulong? Length { get; }
+    }
+}
